9eaf5e0fbc0100313956e4dca97a9cd84931db22
Berglind D. Sigurdardottir <berglinds14@ru.is>
2016-10-10 17:33:35 +0000

    Add files via upload

---
+++ isPrime/main.cpp
@@ -0,0 +1,41 @@
+// 26.08.2015
+// Checks if a number is a prime nunmber
+#include <iostream>
+
+using namespace std;
+
+bool isNumbPrime(int numb);
+
+int main()
+{
+    int numb;
+    bool prime;
+
+    cin >> numb;
+    prime = isNumbPrime(numb);
+
+    if (prime == true)
+        cout << numb << " is a prime number!" << endl;
+    else
+        cout << numb << " is not a prime number!" << endl;
+
+    return 0;
+}
+
+bool isNumbPrime(int numb)
+{
+    int n = 2;
+    bool match = true;
+
+    while((n < numb) && (match == true))
+    {
+        if ((numb % n) == 0)
+        {
+            match = false;
+        }
+
+        n += 1;
+    }
+
+    return match;
+}
