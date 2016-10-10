832a9d8c709a1c77fb936ad8e4a4c3cae74f8442
Berglind D. Sigurdardottir <berglinds14@ru.is>
2016-10-10 20:37:19 +0000

    Add files via upload

---
+++ allEvenNumbs/main.cpp
@@ -0,0 +1,37 @@
+// 04.09.15
+// Input: integer n >= 0
+// Output: all even number from 0 to n
+
+#include <iostream>
+
+using namespace std;
+
+bool isEven(int numb);
+void printEven(int numb);
+
+int main()
+{
+    int numb;
+
+    cin >> numb;
+    printEven(numb);
+
+    return 0;
+}
+
+bool isEven(int numb)
+{
+    if ((numb % 2) == 0)
+        return true;
+
+    return false;
+}
+
+void printEven(int numb)
+{
+    for(int i = 0; i <= numb; i++)
+    {
+        if (isEven(i) == 1)
+            cout << i << endl;
+    }
+}
